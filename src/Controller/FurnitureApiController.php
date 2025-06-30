dfc16a3adac9d4e069d7ac8868846bbc861771c6
luciameneghin <lucia.meneghin00@gmail.com>
2025-06-30 20:47:41 +0200

    feat: backend ecodesign project PHP + Symfony + XAMMP

---
+++ src/Controller/FurnitureApiController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Furniture;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class FurnitureApiController extends AbstractController
+{
+    #[Route('/api/furnitures', name: 'api_furnitures', methods: ['GET'])]
+    public function index(EntityManagerInterface $em): JsonResponse
+    {
+        $furnitures = $em->getRepository(Furniture::class)->findAll();
+
+        $data = [];
+
+        foreach ($furnitures as $furniture) {
+            $data[] = [
+                'id' => $furniture->getId(),
+                'name' => $furniture->getName(),
+                'material' => $furniture->getMaterial(),
+                'price' => $furniture->getPrice(),
+                'image' => 'images' . $furniture->getImage()
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+
+    #[Route('/api/furnitures/populate', name: 'api_furnitures-populate', methods: ['GET'])]
+    public function populate(EntityManagerInterface $em): Response
+    {
+        $furniture1 = new Furniture();
+        $furniture1->setName('Divano Rund');
+        $furniture1->setPrice(98.99);
+        $furniture1->setMaterial('Stoffa riciclata');
+        $furniture1->setColor('Grigio chiaro');
+        $furniture1->setIsGreen(true);
+        $furniture1->setCreatedAt(new \DateTimeImmutable());
+        $furniture1->setImage('public/images/divano-rund-grigino.png');
+
+        $existing = $em->getRepository(Furniture::class)
+            ->findOneBy(['name' => 'Divano Rund']);
+
+        if (!$existing) {
+            $em->persist($furniture1);
+            $em->flush();
+        }
+
+
+        // return $this->json(['message' => 'mobile inserito']);
+        return new Response('Dati inseriti con successo!');
+    }
+}
---
+++ src/Entity/Furniture.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FurnitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FurnitureRepository::class)]
+class Furniture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $material = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $color = null;
+
+    #[ORM\Column]
+    private ?bool $isGreen = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?string $image = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getMaterial(): ?string
+    {
+        return $this->material;
+    }
+
+    public function setMaterial(string $material): static
+    {
+        $this->material = $material;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): static
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function isGreen(): ?bool
+    {
+        return $this->isGreen;
+    }
+
+    public function setIsGreen(bool $isGreen): static
+    {
+        $this->isGreen = $isGreen;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
