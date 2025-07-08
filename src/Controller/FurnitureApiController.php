400636e3cf4950f8cbd02b6d11c716e39964dc69
luciameneghin <lucia.meneghin00@gmail.com>
2025-07-08 22:09:39 +0200

    feat: added cors

--- src/Controller/FurnitureApiController.php
+++ src/Controller/FurnitureApiController.php
@@ -29,13 +29,11 @@ class FurnitureApiController extends AbstractController
                 'price' => $furniture->getPrice(),
                 'isGreen' => $furniture->IsGreen(),
                 'createdAt' => $furniture->getCreatedAt()->format('Y-m-d\TH:i:s'),
-
             ];
         }
 
         return $this->json($data);
     }
-
 
     #[Route('/api/furnitures/populate', name: 'api_furnitures-populate', methods: ['GET'])]
     public function populate(EntityManagerInterface $em): Response
@@ -47,7 +45,7 @@ class FurnitureApiController extends AbstractController
         $furniture1->setColor('Grigio chiaro');
         $furniture1->setIsGreen(true);
         $furniture1->setCreatedAt(new \DateTimeImmutable());
-        $furniture1->setImage('public/images/divano-rund-grigino.png');
+        $furniture1->setImage('images/divano-rund-grigino.png');
 
         $existing = $em->getRepository(Furniture::class)
             ->findOneBy(['name' => 'Divano Rund']);
@@ -57,8 +55,6 @@ class FurnitureApiController extends AbstractController
             $em->flush();
         }
 
-
-        // return $this->json(['message' => 'mobile inserito']);
         return new Response('Dati inseriti con successo!');
     }
 }
